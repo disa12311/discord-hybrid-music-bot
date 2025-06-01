bd4de63d8247b1dce511343136289459750be5c8
disa12311 <hadiepml@gmail.com>
2025-06-01 08:55:05 +0700

    Create client.js

---
+++ src/bot/client.js
@@ -0,0 +1,28 @@
+// src/bot/client.js
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const logger = require('../utils/logger');
+const handleError = require('../utils/errorHandler');
+const helpers = require('../utils/helpers');
+
+module.exports = () => {
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildVoiceStates,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+        ]
+    });
+
+    client.commands = new Collection();
+    client.voicePlayers = new Collection();     // Map: guildId -> AudioPlayer
+    client.musicQueues = new Collection();      // Map: guildId -> Array of songs (object { song_info, textChannelId, requester, _loopMode })
+    client.voiceConnections = new Collection(); // Map: guildId -> VoiceConnection
+
+    // Gắn các utility functions vào client để truy cập dễ dàng
+    client.logger = logger;
+    client.handleError = handleError;
+    client.helpers = helpers;
+
+    return client;
+};
