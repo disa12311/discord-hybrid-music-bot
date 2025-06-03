4c4dbae6688785499ea352ece0a935df28229acf
disa12311 <hadiepml@gmail.com>
2025-06-03 08:26:16 +0700

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -2,13 +2,9 @@
 import { readdirSync } from 'fs';
 import { Collection } from 'discord.js';
 
-/**
- * Loads commands from the 'commands' directory.
- * @param {import('discord.js').Client} client The Discord client.
- */
 export async function loadCommands(client) {
     client.commands = new Collection();
-    client.slashCommands = []; // Mảng để lưu trữ dữ liệu slash commands để đăng ký
+    client.slashCommands = [];
 
     const commandFolders = readdirSync('./commands');
 
@@ -17,10 +13,10 @@ export async function loadCommands(client) {
         for (const file of commandFiles) {
             const { default: command } = await import(`../../commands/${folder}/${file}`);
 
-            if (command.data) { // Đây là Slash Command
+            if (command.data) { // Slash Command
                 client.slashCommands.push(command.data.toJSON());
                 client.commands.set(command.data.name, command);
-            } else if (command.name) { // Đây là Prefix Command
+            } else if (command.name) { // Prefix Command
                 client.commands.set(command.name, command);
             } else {
                 console.warn(`[WARNING] Command at ${folder}/${file} is missing 'data' or 'name' property.`);
