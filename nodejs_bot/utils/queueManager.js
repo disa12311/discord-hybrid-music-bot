1894e6c5ae94d76b9855ac89bc51a91333b7ee58
disa12311 <hadiepml@gmail.com>
2025-06-16 09:28:16 +0700

    Update queueManager.js

--- nodejs_bot/utils/queueManager.js
+++ nodejs_bot/utils/queueManager.js
@@ -1,14 +1,26 @@
 class Queue {
-  constructor() { this.songs = []; this.playing = false; }
-  add(url) { this.songs.push(url); }
-  next() { return this.songs.shift(); }
+  constructor() {
+    this.songs = [];
+    this.playing = false;
+    this.loop = false;
+    this.autoplay = false;
+  }
+
+  add(song) { this.songs.push(song); }
+  next() {
+    if (this.loop && this.songs.length > 0) {
+      this.songs.push(this.songs[0]);
+    }
+    return this.songs.shift();
+  }
   hasNext() { return this.songs.length > 0; }
 }
 
-const guildQueues = new Map();
+const queues = new Map();
+
 module.exports = {
   get(guildId) {
-    if (!guildQueues.has(guildId)) guildQueues.set(guildId, new Queue());
-    return guildQueues.get(guildId);
+    if (!queues.has(guildId)) queues.set(guildId, new Queue());
+    return queues.get(guildId);
   }
 };
