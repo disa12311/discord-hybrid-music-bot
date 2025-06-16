f901aa92c2ef5e8d7ac97ef9b7caf847144e85e9
disa12311 <hadiepml@gmail.com>
2025-06-16 09:07:16 +0700

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// src/config.js
-export const config = {
-    prefix: '!', // Tiền tố cho các lệnh truyền thống (nếu có)
-    embedColor: '#0099ff', // Màu mặc định cho các embed
-    ownerId: '1160073807919317012', // ID của bạn để tạo lệnh owner-only
-    // Thêm các cấu hình khác nếu cần
-};
--- src/events/client/ready.js
+++
@@ -1,8 +0,0 @@
-// events/client/ready.js
-export default {
-    name: 'ready',
-    once: true, // Chỉ chạy một lần khi bot khởi động
-    execute(client) {
-        console.log(`[EVENT] ${client.user.tag} đã sẵn sàng!`);
-    },
-};
