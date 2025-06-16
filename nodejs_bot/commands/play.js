798518f5052a4b3843f25b0ff8edca362d1aadcc
disa12311 <hadiepml@gmail.com>
2025-06-16 09:29:02 +0700

    Update play.js

--- nodejs_bot/commands/play.js
+++ nodejs_bot/commands/play.js
@@ -1,42 +1,50 @@
 const { SlashCommandBuilder } = require('discord.js');
 const ytdl = require('ytdl-core');
-const { joinVoiceChannel, createAudioResource, createAudioPlayer, AudioPlayerStatus } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 const queueManager = require('../utils/queueManager');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Play a song from YouTube')
-    .addStringOption(opt => opt.setName('url').setDescription('YouTube URL').setRequired(true)),
+    .setDescription('Phát bài hát từ YouTube')
+    .addStringOption(opt => opt.setName('url').setDescription('URL').setRequired(true)),
+
   async execute(interaction) {
     const url = interaction.options.getString('url');
     if (!ytdl.validateURL(url)) return interaction.reply('URL không hợp lệ!');
-
     const voiceChannel = interaction.member.voice.channel;
-    if (!voiceChannel) return interaction.reply('Bạn cần trong voice channel');
+    if (!voiceChannel) return interaction.reply('Bạn cần vào voice channel!');
 
     const queue = queueManager.get(interaction.guildId);
     queue.add(url);
+
     if (!queue.playing) {
-      await interaction.reply(`Đang play: ${url}`);
-      playNext(interaction, queue);
+      await interaction.reply(`Đang phát: ${url}`);
+      playNext(interaction, queue, voiceChannel);
     } else {
-      interaction.reply(`Đã thêm vào queue: ${url}`);
+      await interaction.reply(`Đã thêm vào hàng đợi: ${url}`);
     }
   }
 };
 
-function playNext(interaction, queue) {
+async function playNext(interaction, queue, voiceChannel) {
   const url = queue.next();
-  const connection = joinVoiceChannel({ channelId: interaction.member.voice.channelId, guildId: interaction.guildId, adapterCreator: interaction.guild.voiceAdapterCreator });
+  const connection = joinVoiceChannel({
+    channelId: voiceChannel.id,
+    guildId: interaction.guildId,
+    adapterCreator: interaction.guild.voiceAdapterCreator
+  });
   const stream = ytdl(url, { filter: 'audioonly' });
-  const player = createAudioPlayer();
   const resource = createAudioResource(stream);
-  player.play(resource);
+  const player = createAudioPlayer();
   connection.subscribe(player);
+  player.play(resource);
   queue.playing = true;
   player.on(AudioPlayerStatus.Idle, () => {
-    if (queue.hasNext()) playNext(interaction, queue);
-    else { queue.playing = false; connection.destroy(); }
+    if (queue.hasNext()) playNext(interaction, queue, voiceChannel);
+    else {
+      queue.playing = false;
+      connection.destroy();
+    }
   });
 }
