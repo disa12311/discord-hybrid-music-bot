2a9588fa0886710add0bb4d8aa62c9077bd6307f
disa12311 <hadiepml@gmail.com>
2025-06-16 09:30:08 +0700

    Update music.py

--- python_bot/cogs/music.py
+++ python_bot/cogs/music.py
@@ -4,12 +4,13 @@ from discord.ext import commands
 class Music(commands.Cog):
     def __init__(self, bot): self.bot = bot
 
-    @commands.slash_command(name='play', description='Play a song')
+    @commands.slash_command(name='play', description='Phát nhạc')
     async def play(self, ctx, url: str):
-        if not ctx.author.voice: return await ctx.respond('Bạn cần vào voice!')
-        player: wavelink.Player = ctx.voice_client or await ctx.author.voice.channel.connect(cls=wavelink.Player)
+        if not ctx.author.voice:
+            return await ctx.respond('Bạn cần vào voice channel!')
+        vc: wavelink.Player = ctx.voice_client or await ctx.author.voice.channel.connect(cls=wavelink.Player)
         track = await wavelink.YouTubeTrack.search(query=url, return_first=True)
-        await player.play(track)
+        await vc.play(track)
         await ctx.respond(f'Đang phát: {track.title}')
 
 def setup(bot): bot.add_cog(Music(bot))
