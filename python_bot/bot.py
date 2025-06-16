f42fe4974c76b83b4e7a99f8c38d3c90079efb75
disa12311 <hadiepml@gmail.com>
2025-06-16 09:29:30 +0700

    Update bot.py

--- python_bot/bot.py
+++ python_bot/bot.py
@@ -10,12 +10,15 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot ready: {bot.user}')
-    # Khởi Lavalink node
-    await wavelink.NodePool.create_node(bot=bot, host='lavalink', port=2333, password='youshallnotpass')
+    print(f'Bot Python sẵn sàng: {bot.user}')
+    await wavelink.NodePool.create_node(
+        bot=bot,
+        host='lavalink',
+        port=2333,
+        password='youshallnotpass'
+    )
 
-# Load cogs
-for cog in ['music', 'controls']:
+for cog in ['music', 'controls', 'playlist', 'stats', 'logging', 'karaoke']:
     bot.load_extension(f'cogs.{cog}')
 
 bot.run(os.getenv('TOKEN') or TOKEN)
